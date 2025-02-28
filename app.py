ba8aeb9b3bb1a8ed082964a3820a5d245911febd
Alex Belov <alex@cornel.su>
2025-02-28 22:28:25 +0300

    Update

--- app.py
+++ app.py
@@ -240,4 +240,4 @@ def download_file(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=80)
